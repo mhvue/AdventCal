6107183fcdc6f0051b839964901d116544ebdd42
Mai Houa Vue <mhvue4@gmail.com>
2021-01-31 15:56:07 -0600

    added decApp.js which is a copy of app.js so that includes all functionality for when it is Dec. this way, app.js is clean and doesn't have all the commented out functionality.

--- public/decApp.js
+++ public/decApp.js
@@ -1,3 +1,5 @@
+//COPY OF APP.JS for use when it is December. so app.js is cleaner without all the commented out functionality 
+
 //get the date 
 const date = new Date();
 //get the year
@@ -42,9 +44,30 @@ for(let i = -2; i <= 32; i++){
 
 }
 
+//highlight today's date..this is only for when it is December. *Comment out then.
+// function getToday(){
+//     const getToday = moment().format("MM/D/YYYY");
+//     if(getToday == $("#td"+moment().format("D")).attr("data-date")){
+//         $("#td"+moment().format("D")).addClass("clickMe")
+//     }
+    
+
+// }
+// getToday();
+
 //display facts for any day that is clicked on 
 $("td").on("click",function(){
+    //$this is grabbing the id per day 
+//these below are only for when it is December 
+//     const selectDate = $(this).attr("data-date");
+//     const getNum= $(this).html();
+//     const currentDate = moment();
+
+//commented out below as are not getting the current day anymore to show the specific fact for testing purposes 
+// const getD = currentDate.format("D"); //get the current no. day 
+// console.log(getD)
     const getID= $(this).attr("id");
+    console.log(getID)
     
    if(getID){
      $.get("/api/fact/"+ parseInt(getID), function(data){
@@ -105,6 +128,63 @@ $("td").on("click",function(){
         $(".dinoFactHolder").html("nothing here")
    }
     
+ 
+//as of 1/3/21: below will not work as it is for Dec. 2020. NEED TO UPDATE WITH NEW CODE WITH DB
+//check if what is clicked matched today's date
+//current date 
+// if(selectDate == currentDate.format("MM/D/YYYY")){
+//     $(".msgModal").modal();
+//     $("#"+getID).removeClass("clickMe").addClass("clicked")
+//     //show match getNum to match index-1
+//     $(".modal-title").html("Fact For the Day for " + selectDate)
+//     console.log(getD)
+
+//     $.get("/api/dinosaurFact/"+ parseInt(getD), function(data){
+//        // console.log(data.facts)
+          
+//        $(".dinoFactHolder").html(data.facts)
+//     });
+
+// }
+//  //previous date
+// //whatever is clicked on, have to check to see if less than current num
+// else if(parseInt(getNum) < parseInt(getD)){
+//     const prevDay = parseInt(getNum);
+//     $(".msgModal").modal();
+//     $("#"+getID).addClass("clicked")
+//     $(".modal-title").html("Fact For the Day for " + selectDate)
+
+//     $.get("/api/dinosaurFact/"+ prevDay, function(data){
+//         console.log(data,  data.dinoLink.links)
+//         const infoFact = data.facts;
+//         const infoLink = data.dinoLink.links;
+
+//         if(infoLink === null){
+//             $(".dinoFactHolder").html(infoFact)
+//         }
+//         else{
+//             $(".dinoFactHolder").html(infoFact + " "+ 
+//             "<a href=" + infoLink + " target='_blank'>Click Here</a>")
+//         }
+     
+//      });
+     
+// }
+
+//for areas with no dates/num on it  
+// else if(getNum == ""){
+//     $(".msgModal").modal();
+//     $(".dinoFactHolder").html("No date here");
+//     $(".modal-title").html("")
+
+// }
+// //future dates = cannot show 
+// else{
+//     $(".msgModal").modal();
+//     $(".modal-title").html("")
+//     $(".dinoFactHolder").html("Not time to see this fact yet");
+// }
+ 
 });
 
 
